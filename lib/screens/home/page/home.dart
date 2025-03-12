4c99a0ff70873bd20f9f712bc4d1652b9012ab95
emtiaz1 <rafid15-5151@diu.edu.bd>
2025-03-12 10:35:37 +0600

    basicSkeleton2

--- lib/screens/home/page/home.dart
+++ lib/screens/home/page/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:icons_plus/icons_plus.dart';
+import '../../../utils/constans/colors.dart';
 
 class home extends StatelessWidget {
   const home({super.key});
@@ -7,12 +9,25 @@ class home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Mega Shop',
-            style: TextStyle(
-              fontFamily: 'DmSans',
-              fontWeight: FontWeight.w700,
-            )),
+        title: Text(
+          'Mega Shop',
+          style: TextStyle(
+            fontFamily: 'DmSans',
+            fontWeight: FontWeight.w700,
+            color: FColors.oceanBlue,
+          ),
+        ),
         centerTitle: true,
+        actions: [
+          Container(
+            padding: EdgeInsets.fromLTRB(10, 10, 5, 10),
+            child: Icon(Iconsax.notification_bing_outline),
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(10, 10, 5, 10),
+            child: Icon(IonIcons.cart),
+          ),
+        ],
       ),
     );
   }
