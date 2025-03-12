08693145234de725d87f0c227d34cd4846083e6b
emtiaz1 <rafid15-5151@diu.edu.bd>
2025-03-12 10:47:13 +0600

    separate appbar 2

---
+++ lib/common/widgets/appbar.dart
@@ -0,0 +1,35 @@
+import 'package:demo_ecommerce/utils/constans/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:icons_plus/icons_plus.dart';
+class DAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const DAppBar({
+    super.key,
+  });
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text(
+        'Mega Shop',
+        style: TextStyle(
+          fontFamily: 'DmSans',
+          fontWeight: FontWeight.w700,
+          color: DColors.oceanBlue,
+        ),
+      ),
+      centerTitle: true,
+      actions: [
+        Container(
+          padding: EdgeInsets.fromLTRB(10, 10, 5, 10),
+          child: Icon(Iconsax.notification_bing_outline),
+        ),
+        Container(
+          padding: EdgeInsets.fromLTRB(10, 10, 5, 10),
+          child: Icon(IonIcons.cart),
+        ),
+      ],
+    );
+  }
+}
--- lib/screens/home/page/home.dart
+++ lib/screens/home/page/home.dart
@@ -1,3 +1,4 @@
+import 'package:demo_ecommerce/common/widgets/appbar.dart';
 import 'package:flutter/material.dart';
 import 'package:icons_plus/icons_plus.dart';
 import '../../../utils/constans/colors.dart';
@@ -13,35 +14,4 @@ class home extends StatelessWidget {
   }
 }
 
-class DAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const DAppBar({
-    super.key,
-  });
-  @override
-  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 
-  @override
-  Widget build(BuildContext context) {
-    return AppBar(
-      title: Text(
-        'Mega Shop',
-        style: TextStyle(
-          fontFamily: 'DmSans',
-          fontWeight: FontWeight.w700,
-          color: DColors.oceanBlue,
-        ),
-      ),
-      centerTitle: true,
-      actions: [
-        Container(
-          padding: EdgeInsets.fromLTRB(10, 10, 5, 10),
-          child: Icon(Iconsax.notification_bing_outline),
-        ),
-        Container(
-          padding: EdgeInsets.fromLTRB(10, 10, 5, 10),
-          child: Icon(IonIcons.cart),
-        ),
-      ],
-    );
-  }
-}
