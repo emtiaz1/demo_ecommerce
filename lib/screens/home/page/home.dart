603136d92d945dfa268146173a812e45bef3f05e
emtiaz1 <rafid15-5151@diu.edu.bd>
2025-03-12 10:09:12 +0600

    basicSkeleton

--- lib/screens/home/page/home.dart
+++ lib/screens/home/page/home.dart
@@ -11,7 +11,6 @@ class home extends StatelessWidget {
             style: TextStyle(
               fontFamily: 'DmSans',
               fontWeight: FontWeight.w700,
-              
             )),
         centerTitle: true,
       ),
